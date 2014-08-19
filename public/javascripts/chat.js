16fe1b59e7ed34161b49b39f73c2a3beaa63b7de
Constance Jiang <conzjiang@gmail.com>
2014-08-19 09:45:26 -0400

    Can send messages.

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -3,5 +3,9 @@
 
   var Chat = NodeFun.Chat = function (socket) {
     this.socket = socket;
-  }
+  };
+
+  Chat.prototype.sendMessage = function (message) {
+    this.socket.emit("message", { text: message });
+  };
 })(this);
--- public/javascripts/chat_ui.js
+++ public/javascripts/chat_ui.js
@@ -9,8 +9,41 @@
   }
 
   ChatUI.prototype.bindEvents = function () {
+    var ui = this;
+
     this.socket.on("welcome", function (data) {
       $(".chat-box").append("<p>" + data.text + "</p>");
-    })
+    });
+
+    $("#chat-form").on("submit", function () {
+      event.preventDefault();
+      ui._handleMessage();
+    });
+
+    this.socket.on("sendMessage", function (data) {
+      $(".chat-box").append("<p>" + data.text + "</p>");
+    });
+  };
+
+  ChatUI.prototype._handleMessage = function () {
+    var message = $(event.currentTarget).find("textarea").val();
+    var messageText = this._escape(message);
+
+    this.chat.sendMessage(messageText);
+
+    $(event.currentTarget).find("textarea").val("");
+  };
+
+  ChatUI.prototype._escape = function (message) {
+    var msg;
+
+    try {
+      msg = $(message).text();
+    }
+    catch(e) {
+      msg = message;
+    }
+
+    return msg ? msg : message;
   };
 })(this);
