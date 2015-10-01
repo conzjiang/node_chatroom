87e017e0b4f54a27619087c8f844e728a32b30f3
Constance Jiang <conzjiang@gmail.com>
2015-10-01 18:00:41 -0400

    Get rid of $#findByDataId.

--- public/javascripts/utils.js
+++ public/javascripts/utils.js
@@ -1,9 +1,3 @@
 $.fn.scrollToBottom = function () {
   $(this).scrollTop($(this).scrollTop() + $(this).height());
 };
-
-$.fn.findByDataId = function (id) {
-  return this.filter(function () {
-    return $(this).data("id") === id;
-  });
-};
